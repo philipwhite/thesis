3a44b9bb970366793d82397b1b5def78f2250de1
Philip White <philipwhite@mac.com>
2011-07-13 21:57:28 -0500

    updated project.cl, and added .gitignore (forgot to do so last commit)

--- code/project.clj
+++ code/project.clj
@@ -5,6 +5,6 @@
 		 [org.clojars.gilesc/stanford-parser "1.6.7"]
 		 [org.clojars.magnusrw/dependensee "1.4.2"]
 		 [incanter "1.2.3"]
-		 [org.clojars.bmabey/clj-ml "0.1.0"]]
+		 [com.leadtune/clj-ml "0.1.1-SNAPSHOT"]]
   :dev-dependencies [ [swank-clojure "1.3.0"] ]
   :jvm-opts ["-Xmx1g"])
